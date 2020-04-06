38fbf9016564c9474621002dcc834fca206390c5
Anthony Sturdy <35777485+AnthonySturdy@users.noreply.github.com>
2020-04-07 00:58:01 +0100

    Loads all files into memory, working on progress bar

---
+++ Parallelised_Order_Processing/Parallelised_Order_Processing/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Parallelised_Order_Processing {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.LoadDataDirectoryButton = new System.Windows.Forms.Button();
+            this.SelectFolderLabel = new System.Windows.Forms.Label();
+            this.DataDirectoryTextBox = new System.Windows.Forms.RichTextBox();
+            this.SelectStoreInfoTextBox = new System.Windows.Forms.RichTextBox();
+            this.SelectStoreInfoLabel = new System.Windows.Forms.Label();
+            this.SelectStoreInfoButton = new System.Windows.Forms.Button();
+            this.LoadDataButton = new System.Windows.Forms.Button();
+            this.FileLoadingProgressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // LoadDataDirectoryButton
+            // 
+            this.LoadDataDirectoryButton.BackgroundImage = global::Parallelised_Order_Processing.Properties.Resources.folder_1;
+            this.LoadDataDirectoryButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.LoadDataDirectoryButton.Location = new System.Drawing.Point(12, 26);
+            this.LoadDataDirectoryButton.Margin = new System.Windows.Forms.Padding(0);
+            this.LoadDataDirectoryButton.Name = "LoadDataDirectoryButton";
+            this.LoadDataDirectoryButton.Size = new System.Drawing.Size(32, 32);
+            this.LoadDataDirectoryButton.TabIndex = 0;
+            this.LoadDataDirectoryButton.UseVisualStyleBackColor = true;
+            this.LoadDataDirectoryButton.Click += new System.EventHandler(this.LoadDataDirectoryButton_Click);
+            // 
+            // SelectFolderLabel
+            // 
+            this.SelectFolderLabel.AutoSize = true;
+            this.SelectFolderLabel.Location = new System.Drawing.Point(9, 9);
+            this.SelectFolderLabel.Name = "SelectFolderLabel";
+            this.SelectFolderLabel.Size = new System.Drawing.Size(124, 13);
+            this.SelectFolderLabel.TabIndex = 1;
+            this.SelectFolderLabel.Text = "Select Order Data Folder";
+            // 
+            // DataDirectoryTextBox
+            // 
+            this.DataDirectoryTextBox.Location = new System.Drawing.Point(47, 26);
+            this.DataDirectoryTextBox.Multiline = false;
+            this.DataDirectoryTextBox.Name = "DataDirectoryTextBox";
+            this.DataDirectoryTextBox.ReadOnly = true;
+            this.DataDirectoryTextBox.Size = new System.Drawing.Size(298, 32);
+            this.DataDirectoryTextBox.TabIndex = 2;
+            this.DataDirectoryTextBox.Text = "";
+            this.DataDirectoryTextBox.TextChanged += new System.EventHandler(this.DataDirectoryTextBox_TextChanged);
+            // 
+            // SelectStoreInfoTextBox
+            // 
+            this.SelectStoreInfoTextBox.Location = new System.Drawing.Point(396, 26);
+            this.SelectStoreInfoTextBox.Multiline = false;
+            this.SelectStoreInfoTextBox.Name = "SelectStoreInfoTextBox";
+            this.SelectStoreInfoTextBox.ReadOnly = true;
+            this.SelectStoreInfoTextBox.Size = new System.Drawing.Size(298, 32);
+            this.SelectStoreInfoTextBox.TabIndex = 5;
+            this.SelectStoreInfoTextBox.Text = "";
+            this.SelectStoreInfoTextBox.TextChanged += new System.EventHandler(this.SelectStoreInfoTextBox_TextChanged);
+            // 
+            // SelectStoreInfoLabel
+            // 
+            this.SelectStoreInfoLabel.AutoSize = true;
+            this.SelectStoreInfoLabel.Location = new System.Drawing.Point(358, 9);
+            this.SelectStoreInfoLabel.Name = "SelectStoreInfoLabel";
+            this.SelectStoreInfoLabel.Size = new System.Drawing.Size(139, 13);
+            this.SelectStoreInfoLabel.TabIndex = 4;
+            this.SelectStoreInfoLabel.Text = "Select Store Information File";
+            // 
+            // SelectStoreInfoButton
+            // 
+            this.SelectStoreInfoButton.BackgroundImage = global::Parallelised_Order_Processing.Properties.Resources.folder_1;
+            this.SelectStoreInfoButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.SelectStoreInfoButton.Location = new System.Drawing.Point(361, 26);
+            this.SelectStoreInfoButton.Margin = new System.Windows.Forms.Padding(0);
+            this.SelectStoreInfoButton.Name = "SelectStoreInfoButton";
+            this.SelectStoreInfoButton.Size = new System.Drawing.Size(32, 32);
+            this.SelectStoreInfoButton.TabIndex = 3;
+            this.SelectStoreInfoButton.UseVisualStyleBackColor = true;
+            this.SelectStoreInfoButton.Click += new System.EventHandler(this.SelectStoreInfoButton_Click);
+            // 
+            // LoadDataButton
+            // 
+            this.LoadDataButton.Enabled = false;
+            this.LoadDataButton.Location = new System.Drawing.Point(12, 61);
+            this.LoadDataButton.Name = "LoadDataButton";
+            this.LoadDataButton.Size = new System.Drawing.Size(682, 38);
+            this.LoadDataButton.TabIndex = 6;
+            this.LoadDataButton.Text = "Load Data";
+            this.LoadDataButton.UseVisualStyleBackColor = true;
+            this.LoadDataButton.Click += new System.EventHandler(this.LoadDataButton_Click);
+            // 
+            // FileLoadingProgressBar
+            // 
+            this.FileLoadingProgressBar.Location = new System.Drawing.Point(12, 106);
+            this.FileLoadingProgressBar.Name = "FileLoadingProgressBar";
+            this.FileLoadingProgressBar.Size = new System.Drawing.Size(682, 23);
+            this.FileLoadingProgressBar.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 450);
+            this.Controls.Add(this.FileLoadingProgressBar);
+            this.Controls.Add(this.LoadDataButton);
+            this.Controls.Add(this.SelectStoreInfoTextBox);
+            this.Controls.Add(this.SelectStoreInfoLabel);
+            this.Controls.Add(this.SelectStoreInfoButton);
+            this.Controls.Add(this.DataDirectoryTextBox);
+            this.Controls.Add(this.SelectFolderLabel);
+            this.Controls.Add(this.LoadDataDirectoryButton);
+            this.Name = "Form1";
+            this.Text = "Order Data Visualisation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadDataDirectoryButton;
+        private System.Windows.Forms.Label SelectFolderLabel;
+        private System.Windows.Forms.RichTextBox DataDirectoryTextBox;
+        private System.Windows.Forms.RichTextBox SelectStoreInfoTextBox;
+        private System.Windows.Forms.Label SelectStoreInfoLabel;
+        private System.Windows.Forms.Button SelectStoreInfoButton;
+        private System.Windows.Forms.Button LoadDataButton;
+        private System.Windows.Forms.ProgressBar FileLoadingProgressBar;
+    }
+}
+
---
+++ Parallelised_Order_Processing/Parallelised_Order_Processing/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parallelised_Order_Processing {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void LoadDataDirectoryButton_Click(object sender, EventArgs e) {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+            if(result == DialogResult.OK) {
+                DataDirectoryTextBox.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void SelectStoreInfoButton_Click(object sender, EventArgs e) {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "CSV files (*.csv)|*.csv";
+            DialogResult result = ofd.ShowDialog();
+            if(result == DialogResult.OK) {
+                SelectStoreInfoTextBox.Text = ofd.FileName;
+            }
+        }
+
+        private void DataDirectoryTextBox_TextChanged(object sender, EventArgs e) {
+            // Ensure both file directory boxes are not empty
+            if (!string.IsNullOrWhiteSpace(DataDirectoryTextBox.Text) && !string.IsNullOrWhiteSpace(SelectStoreInfoTextBox.Text)) {
+                LoadDataButton.Enabled = true;
+            } else {
+                LoadDataButton.Enabled = false;
+            }
+        }
+
+        private void SelectStoreInfoTextBox_TextChanged(object sender, EventArgs e) {
+            // Ensure both file directory boxes are not empty
+            if (!string.IsNullOrWhiteSpace(DataDirectoryTextBox.Text) && !string.IsNullOrWhiteSpace(SelectStoreInfoTextBox.Text)) {
+                LoadDataButton.Enabled = true;
+            } else {
+                LoadDataButton.Enabled = false;
+            }
+        }
+
+        private void LoadDataButton_Click(object sender, EventArgs e) {
+            // Store information loaded from files
+            ConcurrentDictionary<string, Store> stores = new ConcurrentDictionary<string, Store>();
+            ConcurrentDictionary<Date, byte> dates = new ConcurrentDictionary<Date, byte>();
+            ConcurrentDictionary<Supplier, byte> suppliers = new ConcurrentDictionary<Supplier, byte>();
+            List<Order> orders = new List<Order>();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            // Load store data
+            string[] storeData = File.ReadAllLines(SelectStoreInfoTextBox.Text);
+            Parallel.ForEach(storeData, line => ProcessStoreDataLine(line, ref stores));
+
+            // Load order data
+            string[] fileNames = Directory.GetFiles(DataDirectoryTextBox.Text);
+            FileLoadingProgressBar.Maximum = fileNames.Length;
+            if (fileNames.Length == 0)
+                MessageBox.Show("Can't find any order data in specified file path", "Invalid File Path", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            Parallel.ForEach(fileNames, path => ProcessOrderData(path, ref stores, ref dates, ref suppliers, ref orders));
+
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+
+            int x = 0;
+        }
+
+        void ProcessStoreDataLine(string line, ref ConcurrentDictionary<string, Store> stores) {
+            string[] splitLine = line.Split(',');
+            Store store = new Store();
+            store.code = splitLine[0];
+            store.location = splitLine[1];
+            if (!stores.ContainsKey(store.code))
+                stores.TryAdd(store.code, store);
+        }
+
+        void ProcessOrderData(string path, ref ConcurrentDictionary<string, Store> stores, ref ConcurrentDictionary<Date, byte> dates, ref ConcurrentDictionary<Supplier, byte> suppliers, ref List<Order> orders) {
+            if (Path.GetExtension(path) != ".csv")
+                return;
+
+            // Get stripped filename and split into StoreCode, Week and Year
+            string fileName = Path.GetFileNameWithoutExtension(path.Split('\\').Last());
+            string[] splitFileName = fileName.Split('_');
+
+            // Find which store coresponds to order data
+            Store store = stores[splitFileName[0]];
+
+            // Save date from filename into Date object
+            Date date = new Date();
+            date.week = Convert.ToInt32(splitFileName[1]);
+            date.year = Convert.ToInt32(splitFileName[2]);
+            dates.TryAdd(date, 0x00);
+
+            // Process each line of order data
+            string[] orderData = File.ReadAllLines(path);
+            foreach(string orderLine in orderData) {
+                string[] splitOrderInfo = orderLine.Split(',');
+                Supplier supplier = new Supplier();
+                supplier.name = splitOrderInfo[0];
+                supplier.type = splitOrderInfo[1];
+                suppliers.TryAdd(supplier, 0x00);
+
+                Order order = new Order();
+                order.store = store;
+                order.date = date;
+                order.supplier = supplier;
+                order.cost = Convert.ToDouble(splitOrderInfo[2]);
+                orders.Add(order);
+            }
+
+            // Increment loading bar
+            //if (FileLoadingProgressBar.InvokeRequired) {
+            //    FileLoadingProgressBar.Invoke(new MethodInvoker(delegate { FileLoadingProgressBar.Increment(1); }));
+            //}
+        }
+
+    }
+}
+
+class Store {
+    public String code;
+    public String location;
+}
+
+class Date {
+    public int week;
+    public int year;
+}
+
+class Supplier {
+    public string name;
+    public string type;
+}
+
+class Order {
+    public Store store;
+    public Date date;
+    public Supplier supplier;
+    public double cost;
+}
